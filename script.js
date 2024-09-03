df0e35c81dbd9eded386096ad682b264bea56346
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-03 15:54:27 +0200

    exercise finished + bonus

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const createApp = Vue.createApp
 createApp({
     data() {
         return {
-            message: 'Hello World'
+            message: 'This image goes hard, feel free to screenshot.',
+            imageUrl: 'https://i.imgflip.com/6vhwnx.jpg'
         }
     }
 
 }).mount('#displayer')
+
+
